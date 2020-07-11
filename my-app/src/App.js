d06269dbc142ec5de80991187cec565672d26ffc
dhruvpateldp96 <dhruv_luvsfutbol@yahoo.co.in>
2020-07-11 14:54:58 -0700

    fourth

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import IntervalClassCounter from './components/IntervalClassCounter';
 import IntervalHookCounter from './components/IntervalHookCounter';
 import DataFetching from './components/DataFetching'
+import CounterOne from './components/CounterOne'
+import CounterTwo from './components/CounterTwo'
 
 function App() {
   return (
     <div className="App">
-     <IntervalClassCounter />
+     {/* <IntervalClassCounter />
      <IntervalHookCounter />
      <DataFetching />
+     <CounterOne/> */}
+     <CounterTwo />
     </div>
   );
 }
