e5c3bc0a725d78ec62e61afe52fb14c10b8a9dbc
dhruvpateldp96 <dhruv_luvsfutbol@yahoo.co.in>
2020-07-11 16:27:11 -0700

    context

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import IntervalClassCounter from './components/IntervalClassCounter';
 import IntervalHookCounter from './components/IntervalHookCounter';
 import DataFetching from './components/DataFetching'
 import CounterOne from './components/CounterOne'
 import CounterThree from './components/CounterThree'
+import ComponentA from './components/ComponentA'
+import ComponentB from './components/ComponentB'
+import ComponentC from './components/ComponentC'
 
 function App() {
   return (
     <div className="App">
-     {/* <IntervalClassCounter />
-     <IntervalHookCounter />
-     <DataFetching />
-     <CounterOne/> */}
-     <CounterThree />
+      <ComponentA/>
+      <ComponentB/>
+      <ComponentC/>
+     {/* <CounterThree /> */}
     </div>
   );
 }
