9bbbd98269122107ec88cb74c3449652202c2f40
dhruvpateldp96 <dhruv_luvsfutbol@yahoo.co.in>
2020-07-12 18:34:47 -0700

    reducer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,43 +5,48 @@ import './App.css';
 // import DataFetching from './components/DataFetching'
 // import CounterOne from './components/CounterOne'
 // import CounterThree from './components/CounterThree'
-import ComponentA from './components/ComponentA'
-import ComponentB from './components/ComponentB'
-import ComponentC from './components/ComponentC'
+// import ComponentA from './components/ComponentA'
+// import ComponentB from './components/ComponentB'
+// import ComponentC from './components/ComponentC'
+import DataFetchingTwo from './components/DataFetchingTwo'
 
 
-export const CountContext = React.createContext()
+// export const CountContext = React.createContext()
 
-const initialState = 0
-const reducer = (state, action) => {
-    switch(action) {
-        case 'increment':
-            return state+1
-        case 'decrement':
-            return state-1
-        case 'reset':
-            return initialState           
-        default:
-            return state 
-    }
-}
+// const initialState = 0
+// const reducer = (state, action) => {
+//     switch(action) {
+//         case 'increment':
+//             return state+1
+//         case 'decrement':
+//             return state-1
+//         case 'reset':
+//             return initialState           
+//         default:
+//             return state 
+//     }
+// }
 
 
 
 function App() {
 
-  const [count, dispatch] = useReducer(reducer, initialState)
+  // const [count, dispatch] = useReducer(reducer, initialState)
 
   return (
-    <CountContext.Provider value={{ countState: count, countDispatch: dispatch }}>
-      <div className="App">
-        Count {count}
-        <ComponentA/>
-        <ComponentB/>
-        <ComponentC/>
-      {/* <CounterThree /> */}
+    <div>
+
+      <DataFetchingTwo />
     </div>
-    </CountContext.Provider>
+    // <CountContext.Provider value={{ countState: count, countDispatch: dispatch }}>
+    //   <div className="App">
+    //     Count {count }
+    //     <ComponentA/>
+    //     <ComponentB/>
+    //     <ComponentC/>
+    //   {/* <CounterThree /> */}
+    // </div>
+    // </CountContext.Provider>
 
   );
 }
