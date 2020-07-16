113c7731f5b4304d53a0fe9f7bbe083c5ea59c0f
dhruvpateldp96 <dhruv_luvsfutbol@yahoo.co.in>
2020-07-15 21:18:06 -0700

    list_view

--- Twitter/tweets/views.py
+++ Twitter/tweets/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request,"pages/home.html", context = {},status = 200)
+    return render(request,"tweets/home.html", context = {},status = 200)
 
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{ "id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
 
 def tweet_detail_view(request,tweet_id,*args,**kwargs):
     try:
