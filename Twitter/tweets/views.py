c29aa5958cd0eb0d0534942bd615cd3a640ac193
dhruvpateldp96 <dhruv_luvsfutbol@yahoo.co.in>
2020-07-16 12:44:22 -0700

    nothing

--- Twitter/tweets/views.py
+++ Twitter/tweets/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
-
+import random
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request,"tweets/home.html", context = {},status = 200)
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{ "id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{ "id": x.id, "content": x.content, "likes":random.randint(0,3)} for x in qs]
     data = {
         "response": tweets_list
     }
