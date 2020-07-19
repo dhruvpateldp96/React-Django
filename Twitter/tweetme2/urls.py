1f12b1b074cc830d76914249c4f84aa305db5e59
dhruvpateldp96 <dhruv_luvsfutbol@yahoo.co.in>
2020-07-19 16:58:33 -0700

    react django merge

--- Twitter/tweetme2/urls.py
+++ Twitter/tweetme2/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path, include
 from tweets.views import (home_view, 
@@ -34,3 +36,7 @@ urlpatterns = [
 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+     document_root=settings.STATIC_ROOT)
