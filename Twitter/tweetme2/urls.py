0a130960b3c4801a180b86cf7b6079ecc8250813
dhruvpateldp96 <dhruv_luvsfutbol@yahoo.co.in>
2020-07-19 23:12:29 -0700

    React connection with Django

--- Twitter/tweetme2/urls.py
+++ Twitter/tweetme2/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -27,6 +28,7 @@ from tweets.views import (home_view,
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('react/', TemplateView.as_view(template_name='react.html')),
     path('tweets/<int:tweet_id>',tweet_detail_view),
     path('tweets',tweet_list_view),
     path('create-tweets',tweet_create_view),
